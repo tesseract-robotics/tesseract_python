24fdc6c8b1f832afbbfa280afc8ce49519afb355
John Wason <wason@wasontech.com>
2022-04-25 22:49:13 -0400

    Add environment event callback wrappers

--- tesseract_python/tests/tesseract_environment/test_tesseract_environment.py
+++ tesseract_python/tests/tesseract_environment/test_tesseract_environment.py
@@ -10,6 +10,7 @@ from ..tesseract_support_resource_locator import TesseractSupportResourceLocator
 import traceback
 import os
 import re
+import numpy as np
 
 def get_scene_graph():
     tesseract_support = os.environ["TESSERACT_SUPPORT_DIR"]
@@ -40,6 +41,42 @@ def get_environment():
     success = env.init(scene_graph,srdf)
     assert success
     assert env.getRevision() == 3
+    
+    joint_names = [f"joint_a{i+1}" for i in range(7)]
+    joint_values = np.array([1,2,1,2,1,2,1],dtype=np.float64)
+
+    scene_state_changed = [False]
+    command_applied = [False]
+
+    def event_cb_py(evt):
+        try:
+            if evt.type == tesseract_environment.Events_SCENE_STATE_CHANGED:
+                evt2 = tesseract_environment.cast_SceneStateChangedEvent(evt)
+                if len(evt2.state.joints) != 7:
+                    print("joint state length error")
+                    return
+                for i in range(len(joint_names)):
+                    if evt2.state.joints[joint_names[i]] != joint_values[i]:
+                        print("joint value mismatch")
+                        return
+                scene_state_changed[0] = True
+            if evt.type == tesseract_environment.Events_COMMAND_APPLIED:
+                evt2 = tesseract_environment.cast_CommandAppliedEvent(evt)
+                print(evt2.revision)
+                if evt2.revision == 4:
+                    command_applied[0] = True
+        except:
+            traceback.print_exc()
+    event_cb = tesseract_environment.EventCallbackFn(event_cb_py)
+
+    env.addEventCallback(12345, event_cb)
+
+    env.setState(joint_names, joint_values)
+    assert scene_state_changed[0]
+
+    cmd = tesseract_environment.RemoveJointCommand("joint_a7-tool0")
+    assert env.applyCommand(cmd)
+    assert command_applied[0]
 
     # env.init() now populates contact managers?
 
