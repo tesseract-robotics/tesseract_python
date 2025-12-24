492e6d85469fea23263546e288f02e32d2012c57
Jelle Feringa <jelleferinga@gmail.com>
2025-12-24 12:57:10 +0100

    run_tests.sh: parallel execution with pytest-xdist (-n auto)

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Run all pytests with correct environment
+# Uses pytest-xdist for parallel execution: -n auto (or -n 4, etc)
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -7,4 +8,4 @@ cd "$SCRIPT_DIR"
 
 source env.sh
 
-exec pytest tesseract_nanobind/tests "$@"
+exec pytest tesseract_nanobind/tests -n auto "$@"
