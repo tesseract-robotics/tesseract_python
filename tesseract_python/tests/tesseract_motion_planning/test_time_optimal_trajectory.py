e1e3e17bdd6bc5cfd980d82ec1b6a7f7740d5a40
John Wason <wason@wasontech.com>
2026-05-11 00:24:24 -0400

    Fix test_time_optimal_trajectory.py array dimensions

--- tesseract_python/tests/tesseract_motion_planning/test_time_optimal_trajectory.py
+++ tesseract_python/tests/tesseract_motion_planning/test_time_optimal_trajectory.py
@@ -30,18 +30,18 @@ def create_straight_trajectory():
 
     num = 10
     max_ = 2.0
-    joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"]
+    joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6", "joint_7"]
 
     program = CompositeInstruction()
 
     for i in range(num):
-        p = np.zeros((6,),dtype=np.float64)
+        p = np.zeros((7,),dtype=np.float64)
         p[0] = i * (max_/num)
         swp = StateWaypoint(joint_names, p)        
         program.append(InstructionPoly_wrap_MoveInstruction(MoveInstruction(
             WaypointPoly_wrap_StateWaypoint(swp), MoveInstructionType_FREESPACE)))
 
-    p = np.zeros((6,),dtype=np.float64)
+    p = np.zeros((7,),dtype=np.float64)
     p[0] = max_
     swp = StateWaypoint(joint_names, p)
     program.append(InstructionPoly_wrap_MoveInstruction(MoveInstruction(
@@ -58,9 +58,9 @@ def test_time_parameterization():
     program.setManipulatorInfo(env[1])
     max_velocity = np.array([[2.088, 2.082, 3.27, 3.6, 3.3, 3.078]],dtype=np.float64)
     max_velocity = np.hstack((-max_velocity.T, max_velocity.T))
-    max_acceleration = np.array([[ 1, 1, 1, 1, 1, 1]],dtype=np.float64)
+    max_acceleration = np.array([[ 1, 1, 1, 1, 1, 1, 1]],dtype=np.float64)
     max_acceleration = np.hstack((-max_acceleration.T, max_acceleration.T))
-    max_jerk = np.array([[ 1, 1, 1, 1, 1, 1]],dtype=np.float64)
+    max_jerk = np.array([[ 1, 1, 1, 1, 1, 1, 1]],dtype=np.float64)
     max_jerk = np.hstack((-max_jerk.T, max_jerk.T))
     profile = TimeOptimalTrajectoryGenerationCompositeProfile()
     profile.velocity_limits = max_velocity
